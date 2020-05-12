1ac42bd878624dd28587d6668875c171dda7fa90
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-12 16:45:51 +0300

    covidtaskci

---
+++ story_content/user.js
@@ -0,0 +1,55 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "62knhmQDp9p":
+        Script1();
+        break;
+      case "6J6qEJmBSWu":
+        Script2();
+        break;
+      case "6nSx8cybUAn":
+        Script3();
+        break;
+      case "5yZVxRJ7zBa":
+        Script4();
+        break;
+      case "5tocWQR0upW":
+        Script5();
+        break;
+      case "5VkGp9bP2kE":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script6()
+{
+  window.print();
+}
+
